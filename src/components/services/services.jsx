d6e4d141914dc11e93c0a9f7bb325c6c0ac207c3
SanthushEk <109194698+SanthushEk@users.noreply.github.com>
2025-01-14 22:03:21 +0530

    Initial commit

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Title from "./sections/Title";
 import Events from "./sections/Events";
 import Banner from "./sections/Banner";
-import Brands from "./sections/brands";
+import Brands from "./sections/Brands";
 import Sports from "./sections/Sports";
 import Others from "./sections/Others";
 import Quote from "./sections/Quote";
