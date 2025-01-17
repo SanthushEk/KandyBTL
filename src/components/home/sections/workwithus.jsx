2e18322b32a3814b0ddf160c6fddd895d26e3cd8
SanthushEk <109194698+SanthushEk@users.noreply.github.com>
2025-01-17 15:21:47 +0530

    Initial commit

--- src/components/home/sections/workwithus.jsx
+++ src/components/home/sections/workwithus.jsx
@@ -22,60 +22,60 @@ const WorkWithUs = () => {
         <div className="absolute inset-0 bg-secondary opacity-90 rounded-lg"></div>
 
         {/* Content */}
-        <div className="relative z-10 text-white font-futura border-2 border-gray-500 p-5 lg:pt-24 rounded-sm mb-6 h-full">
+        <div className="relative z-10 text-white font-futura border-2 border-gray-500 p-4 lg:pt-24 rounded-sm mb-6 h-full">
           {/* Title and Subtitle */}
           <div className="relative z-10 text-white pl-0 md:pl-48">
             <h1 className="text-xl md:text-4xl font-semibold mb-2 tracking-wider">
               We Follow The Best Practices
             </h1>
-            <h2 className="text-sm md:text-xl font-light mb-6 tracking-wide ">
+            <h2 className="text-sm md:text-xl font-light mb-6 tracking-wide">
               Brand positioning through Public Private Partnership Programmes
             </h2>
             {/* List Section */}
             <ul className="space-y-4">
-              <li className="flex items-center space-x-3 mb-6">
+              <li className="flex items-center space-x-3 ">
                 <FaLeaf className="text-xl md:text-4xl text-yellow" />
                 <div>
                   <h3 className="text-xl md:text-2xl tracking-wide font-semibold">
                     Sustainability
                   </h3>
-                  <p className="text-sm md:text-lg">
+                  <p className="text-sm md:text-lg hidden sm:block">
                     We prioritize eco-friendly practices in every step of our
                     process.
                   </p>
                 </div>
               </li>
-              <li className="flex items-center space-x-5 mb-6">
+              <li className="flex items-center space-x-5 ">
                 <FaClock className="text-xl md:text-4xl text-yellow" />
                 <div>
                   <h3 className="text-xl md:text-2xl tracking-wide font-semibold">
                     Project On Time
                   </h3>
-                  <p className="text-sm md:text-lg">
+                  <p className="text-sm md:text-lg hidden sm:block">
                     We ensure timely delivery of projects without compromising
                     quality.
                   </p>
                 </div>
               </li>
-              <li className="flex items-center space-x-5 mb-6">
+              <li className="flex items-center space-x-5 ">
                 <FaDesktop className="text-xl md:text-4xl text-yellow" />
                 <div>
                   <h3 className="text-xl md:text-2xl tracking-wide font-semibold">
                     Modern Technology
                   </h3>
-                  <p className="text-sm md:text-lg">
+                  <p className="text-sm md:text-lg hidden sm:block">
                     We leverage the latest technologies for innovative
                     solutions.
                   </p>
                 </div>
               </li>
-              <li className="flex items-center space-x-5 mb-6">
+              <li className="flex items-center space-x-5 ">
                 <FaLightbulb className="text-xl md:text-4xl text-yellow" />
                 <div>
                   <h3 className="text-xl md:text-2xl tracking-wide font-semibold">
                     Creative Concepts
                   </h3>
-                  <p className="text-sm md:text-lg">
+                  <p className="text-sm md:text-lg hidden sm:block">
                     We bring fresh, creative ideas to drive your brand forward.
                   </p>
                 </div>
@@ -86,7 +86,6 @@ const WorkWithUs = () => {
       </motion.div>
 
       {/* Right Section */}
-      {/* Right Section */}
       <motion.div
         className="w-full lg:w-1/2 h-full p-10 lg:pr-36 relative"
         initial={{ opacity: 0, x: isMobile ? 50 : 100 }}
@@ -95,10 +94,10 @@ const WorkWithUs = () => {
         viewport={{ once: true }}
       >
         <div className="relative z-10 mb-6">
-          <h1 className="text-4xl tracking-wider font-bold text-secondary mb-4">
+          <h1 className="text-2xl md:text-4xl tracking-wider font-bold text-secondary mb-4">
             Kandy Sri Lanka
           </h1>
-          <p className="text-lg tracking-wide text-secondary mb-6">
+          <p className="text-sm md:text-lg tracking-wide text-secondary mb-6">
             Kandy BTL's expansive outdoor advertising network covers a wide
             range of impactful platforms, including roadside billboards, bus
             shelter ads, roundabout and iconic locations branding, street
@@ -117,7 +116,7 @@ const WorkWithUs = () => {
               className="w-full h-80 object-cover rounded-lg mb-4"
             />
           </div>
-          <h2 className="text-2xl tracking-wide text-secondary font-semibold mb-4">
+          <h2 className="text-xl md:text-2xl tracking-wide text-secondary font-semibold mb-4">
             10 Years of Undefeated Successes
           </h2>
           <button className="bg-yellow text-secondary px-8 py-2 rounded-lg font-bold tracking-wide transition duration-300">
